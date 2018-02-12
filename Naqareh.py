5d1e5dbf73a9d9da9a5e56ec2004e897d3b163c1
kafura <kafura.kafiri@gmail.com>
2018-02-12 08:31:12 +0330

    i am finisher

--- Naqareh.py
+++ Naqareh.py
@@ -1,16 +1,54 @@
-from flask import Flask, abort, render_template
-from pymongo import MongoClient
 from datetime import datetime
+
 import jwt
+from flask import Flask, abort, render_template, url_for, redirect, jsonify
+from flask_cors import CORS
+from pymongo import MongoClient
+
 import jdate
+
 secret = 'secret'
+admin_password = 'admin'
+admin_username = 'admin'
 db_client = MongoClient()
-db = db_client['days']
+db = db_client['naqareh']
 days = db['days']
 days.drop_indexes()
+# days.delete_many({})
 days.create_index([('key', 1)])
 
 app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/$/<username>:<password>')
+def _admin_key(username, password):
+    if username != admin_username or password != admin_password:
+        abort(403)
+    return jwt.encode({'u': username, 'p': password}, secret, algorithm='HS256')
+
+
+@app.route('/$/<admin_key>/<username>:<password>')
+def new_user(admin_key, username, password):
+    admin_key = admin_key.encode()
+    admin = jwt.decode(admin_key, secret, algorithms=['HS256'])
+    if admin['u'] != admin_username or admin['p'] != admin_password:
+        abort(403)
+    encoded_jwt = jwt.encode({'u': username, 'p': password}, secret, algorithm='HS256')
+    q = {'key': encoded_jwt}
+    day = days.find_one(q)
+    if not day:
+        days.insert_one(q)
+    return encoded_jwt
+
+
+@app.route('/<username>:<password>')
+def get_key(username, password):
+    encoded_jwt = jwt.encode({'u': username, 'p': password}, secret, algorithm='HS256')
+    day = days.find_one({'key': encoded_jwt})
+    if day:
+        return encoded_jwt
+    abort(403)
 
 
 @app.route('/<key>')
@@ -19,13 +57,12 @@ def index(key):
     now = datetime.now()
     jd = jdate.gregorian_to_jd(now.year, now.month, now.day)
     date = jdate.jd_to_persian(jd)
-    user = jwt.decode(key, secret, algorithms=['HS256'])
     q = {
-        'user': user['username'],
+        'key': key,
         'date': {
             'year': date[0],
             'month': date[1],
-            'day': date[2],
+            'day': int(date[2]),
         }
     }
     day = days.find_one(q)
@@ -39,58 +76,56 @@ def index(key):
         ]
         days.insert(day)
     del day['_id']
-    print(day)
-    return day
+    day['key'] = day['key'].decode()
+    return jsonify(day)
 
 
-@app.route('/<key>/<int:year>/<int:month>/<int:day>/<int:hour>:<value>')
-def hour(key, year, month, day, hour, value):
-    user = jwt.decode(key, secret, algorithms=['HS256'])
+@app.route('/<key>/<int:year>/<int:month>/<int:day>/<int:hour>:<int:value>', methods=['POST', 'GET'])
+def set_hour(key, year, month, day, hour, value):
+    key = key.encode()
     q = {
-        'user': user['username'],
+        'key': key,
         'date': {
             'year': year,
             'month': month,
             'day': day
         }
     }
-    try:
-        days.update(q, {
-            '$set': {
-                'hours.{hour}'.format(hour=hour): value
-            }
-        })
+
+    result = days.update(q, {
+        '$set': {
+            'hours.{hour}'.format(hour=hour): value
+        }
+    })
+    if result['updatedExisting']:
         return 'OK', 200
-    except:
-        abort(404)
+    abort(404)
 
 
-@app.route('/<admin>$', methods=['GET'])
-def admin(admin):
-    admin = jwt.decode(admin, secret, algorithms=['HS256'])
+@app.route('/$/<admin_key>', methods=['GET'])
+def admin_index(admin_key):
+    admin_key = admin_key.encode()
+    admin = jwt.decode(admin_key, secret, algorithms=['HS256'])
     if admin['username'] == 'admin':
         return render_template('', )
     abort(403)
 
 
-@app.route('/<admin>$<username>:<password>')
-def new_user(admin, username, password):
-    admin = jwt.decode(admin, secret, algorithms=['HS256'])
-    if admin['username'] == 'admin':
-        encoded_jwt = jwt.encode({'username': username, 'password': password}, secret, algorithm='HS256')
-        return encoded_jwt
-    abort(403)
-
-
-@app.route('/<admin>$<key>', methods=['GET'])
-def user(admin, key):
-    admin = jwt.decode(admin, secret, algorithms=['HS256'])
+@app.route('/$/<admin_key>/<key>', methods=['GET'])
+def get_user(admin_key, key):
+    admin_key = admin_key.encode()
+    key = key.encode()
+    admin = jwt.decode(admin_key, secret, algorithms=['HS256'])
     if admin['username'] == 'admin':
         user = jwt.decode(key, secret, algorithms=['HS256'])
         year = 'year'
     abort(403)
 
 
+@app.route('/favicon.ico')
+def favicon():
+    return redirect(url_for('static', filename='favicon.ico'))
+
+
 if __name__ == '__main__':
-    print(jwt.encode({'username': 'admin', 'password': 'admin'}, secret, algorithm='HS256'))
-    app.run()
+    app.run(debug=True)
--- extension/app/app.js
+++ extension/app/app.js
@@ -1,5 +1,6 @@
 $(function() {
     var template = '#login-template';
+    var base_uri = 'http://localhost:5000'
     if (value = localStorage.apikey) {
         var apikey = value;
         template = '#calendar-template';
@@ -9,6 +10,8 @@ $(function() {
         target: '#target',
         template: template,
         data: {
+            username: '',
+            password: '',
             key: apikey ? apikey : '',
             hours: [
                 0, 1, 0, 2, 1, 0,
@@ -18,14 +21,35 @@ $(function() {
             ],
             date: {
                 year: 1396,
-                month: 'esfand',
-                day: 4
-            }
+                month: 'esfand', // need to pretify() -> ractive
+                day: 4,
+            },
+            format: function(i) {
+                return {
+                    1: 'farvardin',
+                    2: 'ordibehesht',
+                    3: 'khordad',
+                    4: 'tir',
+                    5: 'mordad',
+                    6: 'shahrivar',
+                    7: 'mehr',
+                    8: 'aban',
+                    9: 'azar',
+                    10: 'dei',
+                    11: 'bahman',
+                    12: 'esfand',
+                }[i];
+            },
+            format_label: function(h) {
+                if (h < 10) return '0' + h;
+                return h;
+            },
+            label: 1
         }
     });
 
-    if(ractive.get('key') !== '') {
-        var uri = '/' + ractive.get('key');
+    if(ractive.get('key') != '') {
+        var uri = base_uri + '/' + ractive.get('key');
         $.get(uri, function(response) {
             ractive.set('hours', response.hours);
             ractive.set('date', response.date);
@@ -33,8 +57,13 @@ $(function() {
     }
 
     ractive.on( 'login', function () {
-        alert(this.get('key'));
-        localStorage.apikey = this.get('key');
+        username = this.get('username');
+        password = this.get('password');
+        $.get(base_uri + '/' + username + ':' + password, function(key) {
+            ractive.set('key', key);
+            localStorage.apikey = ractive.get('key');
+            ractive.resetTemplate('#calendar-template');
+        });
     });
 
     ractive.on( 'inc', function(event) {
@@ -44,7 +73,7 @@ $(function() {
         value ++;
         value %= 3;
         var date = ractive.get('date');
-        var uri = '/' +
+        var uri = base_uri + '/' +
             ractive.get('key') + '/' +
             date.year + '/' +
             date.month + '/' +
@@ -53,5 +82,13 @@ $(function() {
         $.post(uri, function(response) {
             ractive.set('hours.' + index, value);
         });
+    });
+
+    ractive.on( 'hour', function(event) {
+        var index = event.node.getAttribute('data-index');
+        index = parseInt(index);
+        hour = (index + 6) % 24;
+        hour -= hour % 6;
+        this.set('label', hour);
     });
 });
